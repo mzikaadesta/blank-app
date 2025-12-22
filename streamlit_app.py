99c57b406c34f1d2d7f480dabdf7a377e50abfc4
mzikaadesta <m.zikaadesta@gmail.com>
2025-12-22 14:17:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ st.title("AWAS ADA UJANGGGGG 🏍️🏍️🏍️🏍️")
 st.write(
     "irsyad TOP GLOBAL 85 EDITH SEASOn 38 [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-.teks-besar {
+.teks-besar {🏍️🏍️🏍️
     font-size: 50px;
 }
