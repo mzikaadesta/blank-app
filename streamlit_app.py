f6147696ed52b81a42d7be8bd276be502c77f0cf
mzikaadesta <m.zikaadesta@gmail.com>
2025-12-22 13:57:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,3 @@ st.title("AWAS ADA UJANGGGGG 🏍️🏍️🏍️🏍️")
 st.write(
     "irsyad TOP GLOBAL 85 EDITH SEASOn 38 [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-import streamlit as st
-st.image('gambar.jpg', caption='Gambar Lokal', width=300)
