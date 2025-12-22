fa3294baf78d20966cc98a5d5c72b1ccc7d983e9
mzikaadesta <m.zikaadesta@gmail.com>
2025-12-22 13:42:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("AWAS ADA UJANGGGGG 🏍️🏍️🏍️🏍️")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
