db4b218c0c936a3da3d55c8182a7a3358b094a48
mzikaadesta <m.zikaadesta@gmail.com>
2025-12-22 13:50:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,16 @@ import streamlit as st
 
 st.title("AWAS ADA UJANGGGGG 🏍️🏍️🏍️🏍️")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "irsyad TOP GLOBAL 85 EDITH SEASOn 38 [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+
+st.title("Upload Foto")
+
+foto = st.file_uploader(
+    "Upload foto",
+    type=["jpg", "jpeg", "png"]
+)
+
+if foto:
+    st.image(foto, caption="Foto yang diunggah", use_container_width=True)
